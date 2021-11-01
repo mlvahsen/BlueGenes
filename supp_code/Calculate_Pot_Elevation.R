749f481f8ad85643859fe698d30ce7fa52cfdab6
mlvahsen <mlvahsen@gmail.com>
2021-11-01 09:52:08 -0500

    file organization and started RMarkdown

---
+++ supp_code/Calculate_Pot_Elevation.R
@@ -0,0 +1,125 @@
+# This script uses both RTK-GPS and hand measurements to get actual heights of
+# pots within frames. We can then use this data as a covariate in models rather
+# than levels. This should overall reduce frame effects as well.
+
+# All measurements taken by Helena Kleiner in Spring 2020
+# Script created 25 June 2020 by MLV
+# Script updated 25 June 2020 by MLV
+
+# Load libraries
+library(tidyverse); library(here)
+
+# Set plotting theme
+theme_set(theme_classic())
+
+# Read in data
+# First data set is RTK-GPS measures of the top rack
+top_level <- read_csv(here("supp_data", "organ_rack_height.csv"))
+# Second data is the differences between racks (e.g. distance between top level
+# and second level from the top)
+level_diffs <- read_csv(here("supp_data", "shelf_heights_measured.csv"))
+
+# Start with top_level and tidy data
+top_level %>% 
+  dplyr::select(id = Point.ID,
+         elev_1 = Elevation_1,
+         elev_2 = Elevation_2,
+         elev_3 = Elevation_3) %>% 
+  # Add 25 cm to all elevation values (screws for all pots but G6 were 30 cm
+  # down and peat was 5 cm down from the top of the pot). Need to add 9 cm to
+  # the measurement of G6 because screws had to be placed lower because we
+  # couldn't move that rack
+  mutate(elev_1 = ifelse(id == "G6", elev_1 + 0.09 + 0.25, elev_1 + 0.25),
+         elev_2 = ifelse(id == "G6", elev_2 + 0.09 + 0.25, elev_2 + 0.25),
+         elev_3 = ifelse(id == "G6", elev_3 + 0.09 + 0.25, elev_3 + 0.25)) %>% 
+  group_by(id) %>% 
+  mutate(mean_elev = mean(c(elev_1, elev_2, elev_3)),
+         sd_elev = sd(c(elev_1, elev_2, elev_3)),
+         se_elev = sd_elev / sqrt(3)) -> top_level_summary
+
+# Plot top levels with standard errors
+top_level_summary %>% 
+  ggplot(aes(x = id, y = mean_elev)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin = mean_elev - se_elev, ymax = mean_elev + se_elev), width = 0.5) +
+  ylab("mean elevation (m NAVD88)")
+
+# We could use the information about variability as an errors in variables
+# problem to be complete about how variation in elevation drives differences in
+# responses, but I think for now we can just take the mean elevation for each
+# rack.
+
+top_level_summary %>% 
+  dplyr::select(id, mean_elev) -> top_level_mean
+
+range(top_level_mean$mean_elev)
+# We have up to ~5cm difference across frames so it's probably good we are
+# accounting for this!
+
+
+
+# Now format the level differences
+level_diffs %>% 
+  # Take out measurements that had "DISREGARD" in the notes
+  filter(!grepl("DISREGARD",level_diffs$notes)) %>% 
+  group_by(site_frame) %>% 
+  # There should be 5 for each frame (1-2, 2-3, 3-4, 4-5, 5-6)
+  dplyr::count() %>% 
+  filter(n != 5)
+
+# Check what's up with gcrew_3 and gcrew_4
+level_diffs %>% 
+  filter(!grepl("DISREGARD",level_diffs$notes)) %>% 
+  filter(site_frame %in% c("gcrew_3", "gcrew_4")) %>% 
+  pull(notes)
+# Ok, it looks like we should average measurements taken with rows that have the
+# same level based on the notes
+
+level_diffs %>% 
+  filter(!grepl("DISREGARD",level_diffs$notes)) %>% 
+  group_by(site_frame, level) %>% 
+  # Convert to meters
+  summarize(measurement_R = mean(measurement_R) * 0.01,
+            measurement_C = mean(measurement_C * 0.01),
+            measurement_L = mean(measurement_L) * 0.01) %>% 
+  group_by(site_frame, level) %>% 
+  # Now take the global mean across right, center and left
+  summarize(mean_diff = mean(c(measurement_R, measurement_C, measurement_L))) %>% 
+  ungroup() %>% 
+  # Rename level to be numeric 2-6, where 2 represents the difference between 1 to 2
+  mutate(level = ifelse(level == "1 to 2", 2,
+                        ifelse(level == "2 to 3", 3,
+                               ifelse(level == "3 to 4", 4,
+                                      ifelse(level == "4 to 5", 5, 6))))) -> level_diffs_sum
+
+# Create storage for loop that will go through and subtract elevations for each
+# step
+storage <- matrix(NA, nrow = 14, ncol = 6)
+
+# Make sure both data frames are sorted before we loop through
+top_level_summary <- top_level_summary[order(top_level_summary$id),]
+level_diffs_sum <- level_diffs_sum[order(level_diffs_sum$site_frame, level_diffs_sum$level),]
+
+# Set vector of frame names for level_diffs
+frame_names <- unique(level_diffs_sum$site_frame)
+
+# Loop through to get elevations for each level for each frame
+for (i in 1:14){
+  temp_top <- top_level_summary$mean_elev[i]
+  temp_rest <- level_diffs_sum %>% filter(site_frame == frame_names[i]) %>% pull(mean_diff)
+  storage[i,1] <- temp_top
+  for (j in 2:6){
+    storage[i,j] <- storage[i,j-1] - temp_rest[j-1]
+  }
+}
+
+# Add back ids and levels and put in a tibble
+all_elevations <- data.frame(storage)
+colnames(all_elevations) <- paste("level", 1:6, sep = "_")
+tibble(all_elevations) %>% 
+  mutate(site_frame = frame_names) %>% 
+  gather(key = level, value = elevation, level_1:level_6) %>% 
+  mutate(level = substr(level, nchar(level), nchar(level))) -> elevation_data
+
+# Remove all previous data frames and keep the last one
+rm(list=setdiff(ls(), "elevation_data"))
---
+++ supp_code/CompileTraitData.R
@@ -0,0 +1,420 @@
+# Compile trait data for Blue Genes
+
+# Blue Genes Analysis -- Clean code!!! #
+
+# Created 14 September 2021
+# Updated 18 October 2021
+
+####################
+## Data Wrangling ##
+####################
+
+## Preliminaries ####
+
+# Load libraries
+library(tidyverse); library(lubridate); library(here)
+
+# Set graphical theme
+theme_set(theme_bw())
+
+# Read in elevation data
+source(here("supp_code", "Calculate_Pot_Elevation.R"))
+
+# Read in initial conditions data
+inits <- read_csv(here("supp_data","InitialConditions_FinalReps_NOEDITS.csv"))
+
+# Read in the rest of the data
+census <- read_csv(here("supp_data","Biweekly_toSept16.csv"))
+abg <- read_csv(here("supp_data","AbovegroundBiomass_data_17Jun20.csv"))
+reap_pot <- read_csv(here("supp_data","Harvest_FullData_PotLevel.csv"))
+reap_stem <- read_csv(here("supp_data","Harvest_FullData_StemLevel.csv"))
+pot_ids <- read_csv(here("supp_data","HarvestPot_ID.csv")) # for matching up pot_no with data sheets that don't have pot_no
+rna_harvest <- read.csv(here("supp_data","RNA_harvest.csv"))
+
+## Data cleaning and formatting ##### 
+
+# Add pot numbers to initial conditions data frame
+inits %>% 
+  rename(level = flood_level) %>% 
+  mutate(id = paste(site_frame, level, position, sep = "_")) -> inits
+
+elevation_data$site_level <- paste(elevation_data$site_frame, elevation_data$level, sep = "_")
+inits$site_level <- paste(inits$site_frame, inits$level, sep = "_")
+
+# Add true elevation to initial conditions data
+merge(elevation_data, inits, by = "site_level") -> inits_elev
+
+# Add pot numbers
+inits_elev %>% 
+  mutate(id = paste(site_level, position, sep = "_")) %>% 
+  select(-site_frame.y, -level.y) %>% 
+  rename(site_frame = site_frame.x,
+         level = level.x) -> inits_elev
+
+pot_ids %>% 
+  mutate(id = paste(`site_ frame`, level, pos, sep = "_")) %>% 
+  select(id, pot_no) -> pot_ids_only
+
+merge(inits_elev, pot_ids_only, by = "id") -> inits_ids
+
+# Add aboveground biomass
+
+# Get total weight of biomass that was weighed in bags
+abg %>% 
+  select(-Date) %>% # Helps deal with the double entry problem
+  select(-Recorder) %>% 
+  filter(notes != "dh rhizome" & notes != "dh stem" | is.na(notes)) %>%  
+  filter(!grepl("dh rhizome",notes)) %>% 
+  select(-notes) %>% 
+  mutate(allom = ifelse(allom == "N/A", NA, allom)) %>% 
+  # Deal with the double entry problem
+  distinct() %>% 
+  group_by(pot_no, species) %>% 
+  summarize(weight = sum(weight)) %>% 
+  spread(key = species, value = weight) %>% 
+  mutate(scam = ifelse(is.na(scam), 0, scam),
+         sppa = ifelse(is.na(sppa), 0, sppa)) -> abg_no_zeros
+
+# Figure out which pots have zero biomass (i.e. no biomass was weighed)
+pot_ids %>% 
+  filter(!pot_no %in% abg_no_zeros$pot_no) %>% 
+  mutate(scam = 0,
+         sppa = 0) %>% 
+  select(pot_no, scam, sppa) -> abg_zeros
+
+# Bind together
+abg_all <- rbind(abg_no_zeros, abg_zeros)
+
+# Merge abg with initial conditions data frame
+full_data <- merge(inits_ids, abg_all, by = "pot_no")
+
+# Match up initial conditions with harvest census
+reap_pot %>% 
+  select(pot_no, scam_live_density, scam_dead_density, max_sppa_height,
+         soil_to_pot, dh_count) %>% 
+  mutate(max_sppa_height = ifelse(max_sppa_height == "N/A", NA, max_sppa_height)) -> reap_sum
+
+# Merge reap_sum with full_data
+full_data <- merge(full_data, reap_sum, by = "pot_no")
+
+# Now deal with pot level harvest data
+# There are some non-numerics in the width data
+reap_stem[is.na(as.numeric(reap_stem$mid_wd)),"mid_wd"]
+
+reap_stem %>% 
+  mutate(mid_wd = as.numeric(ifelse(mid_wd == "N/A", NA, mid_wd)),
+         bas_wd = as.numeric(ifelse(bas_wd == "N/A", NA, bas_wd))) %>% 
+  # Filter out drainage hole stems and those that are cut when calculating
+  # average height and widths
+  filter(dh == 0 & cut == 0) %>% 
+  group_by(pot_no) %>% 
+  summarize(mean_tot_ht = mean(tot_ht, na.rm = T),
+            mean_grn_ht = mean(grn_ht, na.rm = T),
+            mean_mid_wd = mean(mid_wd, na.rm = T),
+            mean_bas_wd = mean(bas_wd, na.rm = T),
+            # Also calculate proportion twisted
+            prop_twist = length(which(twist == 1)) / length(twist)) -> reap_height_sum
+
+# Create rows for data that does not have measurements for scam
+pot_ids %>% 
+  filter(!pot_no %in% reap_height_sum$pot_no) %>% 
+  select(pot_no) %>% 
+  mutate(mean_tot_ht = NA,
+         mean_grn_ht = NA,
+         mean_mid_wd = NA,
+         mean_bas_wd = NA,
+         prop_twist = NA) -> reap_height_zeros
+
+# Bind together data with heights and data with no heights
+reap_all <- rbind(reap_height_sum, reap_height_zeros)
+
+# Merge with full data
+full_data <- merge(full_data, reap_all, by = "pot_no")
+
+# Check to see if we have any instances where we don't have height/width data
+# but do have biomass data
+full_data %>% filter(is.na(mean_tot_ht)) %>% 
+  select(scam_live_density, pot_no) %>% 
+  filter(scam_live_density > 0)
+# Harvest date on this is 13 October so this must be the pot that MLV pulled
+# late that actually had biomass in it
+
+# Check to see where there is biomass but there were no live stems
+full_data[which(full_data$scam != 0 & full_data$scam_live_density == 0),] %>% 
+  select(pot_no, scam, scam_live_density, mean_tot_ht)
+# These are all just dead biomass so this makes sense
+
+# Check to see where there was no biomass but there were live stems
+full_data[which(full_data$scam == 0 & full_data$scam_live_density != 0),] %>% 
+  select(pot_no, scam, scam_live_density, mean_tot_ht)
+# Good!
+
+# Group date_cloned and date_planted data information so it can be used in the
+# models in a meaningful way. Clean up data and select what we want for models.
+full_data %>% 
+  mutate(date_cloned_grp = factor(ifelse(date_cloned %in% c("5/22/19", "5/28/19", "5/27/19"), 1,
+                                         ifelse(date_cloned %in% c("6/9/19", "6/10/19"), 2, 3))),
+         date_planted_grp = factor(ifelse(date_planted %in% c("6/13/19", "6/14/19", "6/17/19"), 1, 2))) %>% 
+  select(pot_no,site_frame,level,position,
+         co2,
+         salinity,
+         comp,
+         elevation,
+         genotype,
+         location,
+         depth_seed = depth,
+         cohort,
+         origin_lab = origin,
+         date_cloned_grp,
+         date_planted_grp,
+         weight_init = weight,
+         width_init = width,
+         nodes_init = nodes,
+         agb_scam = scam,
+         agb_sppa = sppa,
+         dens_scam_live = scam_live_density,
+         dens_scam_dead = scam_dead_density,
+         height_sppa = max_sppa_height,
+         height_scam_tot = mean_tot_ht,
+         height_scam_grn = mean_grn_ht,
+         width_scam_mid = mean_mid_wd,
+         width_scam_bas = mean_bas_wd,
+         prop_twist,
+         soil_to_pot
+  ) -> full_data
+
+## Dealing with zeros #### 
+# We have zeros that arise for two different reasons:
+# (1) plants failed to establish and (2) plants established and then died. For
+# most analyses we want to include the zeros that are where plants established
+# and then died, that is, that the treatment they were exposed to was a reason
+# they died.
+
+# To figure this out, we can use the biweekly census data. First format the
+# sampling dates.
+census$yday <- yday(mdy(census$date))
+
+census$day <- NULL
+census[census$yday %in% 182:183, "day"] <- 1
+census[census$yday %in% 196:197, "day"] <- 2
+census[census$yday %in% 210:212, "day"] <- 3
+census[census$yday %in% 224:226, "day"] <- 4
+census[census$yday %in% 245:248, "day"] <- 5
+census[census$yday %in% 259:261, "day"] <- 6
+
+# Set up ids
+full_data$id <- paste(full_data$site_frame, full_data$level, full_data$position, sep = "_")
+census$id <- paste(census$site_frame, census$level, census$pos, sep = "_")
+
+# Figure out which pots had zero stems at the end of the experiment
+zero_stems <- full_data %>% filter(dens_scam_live == 0)
+
+# Filter those out in census data
+census %>% 
+  filter(id %in% zero_stems$id) %>% 
+  select(id, live, day) -> zero_decision_data
+
+# Now figure out which pots were continuing to increase after certain dates
+zero_decision_data %>% 
+  spread(key = day, value = live) %>% 
+  select(id,
+         day1 = `1`,
+         day2 = `2`,
+         day3 = `3`,
+         day4 = `4`,
+         day5 = `5`,
+         day6 = `6`) %>% 
+  filter(day6 > 0 | day5 > 0 | day4 > 0 | day3 > 0 | day2 > day1) %>% 
+  pull(id) -> zeros_to_keep
+# I think this makes good sense: had stems alive after ~ 1 month (July 29) or
+# was increasing density from July 1 - July 15
+
+## Dealing with RNA stems #### 
+# One stem was harvested for all pots in the rna_harvest data frame. We add one
+# stem to the density count and then use the allometric data to calculate the
+# biomass and then add that to the measured biomass for the rest of the stems.
+
+# Use allometric stems to create a calibration to predict stems that were
+# harvested for RNA
+abg %>% 
+  filter(allom %in% c(1,2)) %>% 
+  mutate(stem_id = paste(pot_no, allom, sep = "_")) %>% 
+  select(stem_id, weight) %>% 
+  # deals with double entry of some biomass data
+  distinct() -> allom_biomass
+
+# Get height and width from census data for allom stems
+# Fix one reap_stem data entry issue
+reap_stem %>% 
+  filter(allom %in% c(1,2)) %>% 
+  mutate(stem_id = paste(pot_no, allom, sep = "_")) %>% 
+  select(stem_id, tot_ht, grn_ht, mid_wd, bas_wd) -> allom_hw
+
+dim(allom_biomass)
+dim(allom_hw) # doesn't quite match up so filter out ids that match up
+
+merge(allom_biomass, allom_hw, by.x = "stem_id") -> allom_data
+
+# Fit log-transformed linear model accounting for heights, widths and interaction
+# between height and width
+hw_mod <- lm(log(weight) ~ tot_ht * mid_wd, data = allom_data)
+#plot(hw_mod)
+# Fit log-transformed linear model accounting for heights only
+h_mod <- lm(log(weight) ~ tot_ht, data = allom_data)
+#plot(h_mod)
+
+# Predict weights of RNA stems
+# Filter data that has heights and widths (all but 2)
+rna_harvest %>% 
+  filter(complete.cases(mid_wd)) -> rna_hw
+# Filter data that only has heights
+rna_harvest %>% 
+  filter(!complete.cases(mid_wd)) -> rna_h
+
+# Predict mass for stems we have height and widths
+rna_hw$biomass <- exp(predict(hw_mod, newdata = rna_hw %>% select(tot_ht, mid_wd)))
+rna_h$biomass <- exp(predict(h_mod, newdata = rna_h %>% select(tot_ht)))
+rna_pred <- rbind(rna_hw, rna_h) 
+
+# Sum biomass and select pot_id and additional biomass column
+rna_pred %>% 
+  group_by(pot_no) %>% 
+  summarize(biomass_add = sum(biomass)) -> rna_pred
+
+# Add additional biomass
+full_data[full_data$pot_no %in% rna_pred$pot_no,"agb_scam"] <- full_data[full_data$pot_no %in% rna_pred$pot_no,"agb_scam"] + rna_pred$biomass_add
+
+# Add in extra density if RNA harvested stem (+1 stem)
+full_data %>% 
+  mutate(dens_scam_live = ifelse(pot_no %in% rna_harvest$pot_no, dens_scam_live + 1, dens_scam_live)) -> full_data
+
+## Belowground ####
+# Read in belowground data
+source(here("supp_code", "bgb_data_fix.R"))
+
+bg_clean %>% 
+  group_by(pot_no) %>% 
+  summarize(total_bg = sum(weight)) -> bg_total
+
+full_data_merged <- merge(bg_total, full_data, by = "pot_no", all.y = T)
+
+# Filter out top 10cm that is SCAM or SPPA only (no mixed roots)
+bg_clean %>% 
+  filter(segment_top == 0 & species %in% c("scam", "sppa")) %>% 
+  group_by(pot_no, species) %>% 
+  summarize(bg_biomass10 = sum(weight)) %>% 
+  spread(key = species, value = bg_biomass10) %>% 
+  ungroup()-> bg_10cm
+
+# Merge in with full_data
+merge(bg_10cm, full_data_merged, by = "pot_no", all.y = T) %>% 
+  rename(bg_scam10 = scam, bg_sppa10 = sppa)-> full_data_merged
+
+# Also calculate root distribution parameters values for non-competition pots
+# Get pot numbers for no competition pots
+full_data %>% 
+  filter(comp == 0) %>% 
+  pull(pot_no)-> no_comp_pots
+
+bg_clean %>% 
+  filter(pot_no %in% no_comp_pots) %>% 
+  # Group everything by layer
+  group_by(pot_no, segment_top) %>% 
+  summarize(biomass = sum(weight))-> bg_nocomp
+
+# Make summary of total biomass and number of layers for each pot
+bg_nocomp %>%
+  group_by(pot_no) %>%  
+  summarize(total = sum(biomass),
+            n = length(biomass)) %>% 
+  # There are a couple pots that had sectioning issues (with just 1-2 sections)
+  # so we drop these
+  filter(n > 2) -> totals
+
+# Figure out which pots we dropped
+bg_nocomp %>%
+  group_by(pot_no) %>%  
+  summarize(total = sum(biomass),
+            n = length(biomass)) %>% 
+  filter(n < 3) %>% pull(pot_no) -> dropped_pots
+
+# Make %notin%
+`%notin%` <- Negate(`%in%`)
+
+# Make sure data is sorted by pot and rooting depth
+bg_nocomp %>%
+  ungroup() %>% 
+  filter(pot_no %notin% dropped_pots) %>% 
+  arrange(pot_no, segment_top) %>% 
+  # Create repeated entries of the total belowground biomass in order to
+  # calculate proportions
+  mutate(total = rep(totals$total, totals$n)) %>% 
+  # Calculate proportions
+  mutate(prop = biomass / total) %>% 
+  # Group by pot so that we can calculate cumulative proportion across depth
+  group_by(pot_no) %>% 
+  mutate(cum_sum = cumsum(prop)) %>% 
+  ungroup() -> bgb_beta_analysis
+
+# Create an additional set of data such that at depth 0, there is no biomass
+bgb_beta_analysis %>% 
+  dplyr::select(pot_no) %>% 
+  unique() -> for_adding_zeros 
+
+# Here depth is -5, but will get changed to 0 in the next step
+for_adding_zeros$segment_top <- -10
+for_adding_zeros$biomass <- 0
+for_adding_zeros$cum_sum <- 0
+
+# Order columns the same way as the for_adding_zeros dataframe
+bgb_beta_analysis %>% 
+  dplyr::select(pot_no, segment_top, biomass, cum_sum)->bgb_beta_analysis
+
+bgb_beta_analysis_zeros <- rbind(for_adding_zeros, bgb_beta_analysis) %>% 
+  mutate(depth_roots = case_when(segment_top == -10 ~ 0,
+                                 segment_top == 0 ~ 10,
+                                 segment_top == 10 ~ 20,
+                                 T ~ 70)) %>% 
+  arrange(pot_no, depth_roots) %>% 
+  select(pot_no, depth_roots, biomass, cum_sum)
+
+## Run non-linear regression on cumulative probability for each pot
+
+# Create output vector to hold information
+beta <- NULL
+
+for (i in 1:length(unique(for_adding_zeros$pot_no))){
+  sub_data <- bgb_beta_analysis_zeros %>% filter(pot_no == unique(for_adding_zeros$pot_no)[i])
+  mod_temp <- nls(cum_sum ~ 1 - beta ^ (depth_roots), data = sub_data, start = list(beta = 1))
+  beta[i] <- coef(mod_temp)[1]
+}
+
+for_adding_zeros$beta <- beta
+
+# Merge together with the rest of the data
+for_adding_zeros %>% 
+  select(pot_no, beta) %>% 
+  # Add NAs for dropped pots
+  add_row(pot_no = dropped_pots[1], beta = NA) %>%
+  add_row(pot_no = dropped_pots[2], beta = NA) %>%
+  add_row(pot_no = dropped_pots[3], beta = NA) %>%
+  arrange(pot_no) -> betas_to_merge
+
+bg_full <- merge(betas_to_merge, full_data_merged, all.y = T, by = "pot_no")
+
+# Create an extinct vs extant binary variable based on SCAM aboveground biomass
+bg_full %>% 
+  mutate(extinct = case_when(agb_scam > 0 ~ 0,
+                             T ~ 1)) %>% 
+  # Also make a variable for age cohort
+  mutate(age = case_when(grepl("ancestral", cohort) ~ "ancestral",
+                         T ~ "modern"))-> bg_full
+
+# Now filter out the "failure to establish" zeros from the full data
+bg_full %>% 
+  filter(id %in% zeros_to_keep | dens_scam_live > 0) -> bg_est
+
+## Remove all ancillary datasets and keep blue_genes data set and full_data.
+## This is just housekeeping so I don't get confused by all of the data frames
+rm(list=setdiff(ls(), c("bg_full")))
+
